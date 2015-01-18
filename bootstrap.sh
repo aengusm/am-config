463e7fa881ba05f0e77704209ed857bd4753462c
Martin <aengusm@amazon.com>
2015-01-18 12:08:07 -0800

    added basic zshrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,11 @@ rm -rf .vim
 rm -f .vimrc
 ln -s am-config/vim .vim
 ln -s .vim/vimrc .vimrc
+
+if [[ -f .zshrc ]]; then
+    if ! [[ -h .zshrc ]]; then
+        cp .zshrc .zshrc_local
+    fi
+fi
+
+ln -s am-config/zsh/zshrc .zshrc
